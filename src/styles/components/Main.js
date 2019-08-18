d6970661ed5b06edf998387eaf8432e5e30810dd
King Larce Cortes <larce@Kings-MacBook-Pro.local>
2019-08-18 14:17:09 +0900

    Add component slowly

---
+++ src/styles/components/Main.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import mixins from '@styles/mixins';
+
+const Main = styled.main`
+  ${mixins.sidePadding};
+  margin: 0 auto;
+  width: 100%;
+`;
+
+export default Main;
--- src/styles/index.js
+++ src/styles/index.js
@@ -2,6 +2,26 @@ import theme from '@styles/theme';
 import globalStyle from '@styles/globalStyle';
 import mixins from '@styles/mixins';
 import mediaQuery from '@styles/mediaQuery';
-import Button from '@styles/components/Button';
 
-export { theme, globalStyle, mixins, mediaQuery, Button };
+// Styled components
+import Header from '@styles/components/Header';
+import Main from '@styles/components/Main';
+import Section from '@styles/components/Section';
+import Footer from '@styles/components/Footer';
+import Heading from '@styles/components/Heading';
+import Button from '@styles/components/Button';
+import InlineLink from '@styles/components/InlineLink';
+
+export {
+  theme,
+  globalStyle,
+  mixins,
+  mediaQuery,
+  Header,
+  Main,
+  Section,
+  Footer,
+  Heading,
+  Button,
+  InlineLink
+};
