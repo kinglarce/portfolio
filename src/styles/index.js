d6ae1fea93cf42ab44c9557e913657aab1e6b861
King Larce Cortes <kingcort01@gmail.com>
2019-08-18 22:30:46 +0900

    Add hero component and update stylings

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,8 +1,8 @@
 import theme from '@styles/theme';
-import globalStyle from '@styles/globalStyle';
 import mixins from '@styles/mixins';
 import mediaQuery from '@styles/mediaQuery';
 
+import GlobalStyle from '@styles/components/GlobalStyle';
 import Header from '@styles/components/Header';
 import Main from '@styles/components/Main';
 import Section from '@styles/components/Section';
@@ -13,9 +13,9 @@ import InlineLink from '@styles/components/InlineLink';
 
 export {
   theme,
-  globalStyle,
   mixins,
   mediaQuery,
+  GlobalStyle,
   Header,
   Main,
   Section,
