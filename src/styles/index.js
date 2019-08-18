d7284da0f951ae8d1afcd23e5a5b9b7a65158bd2
King Larce Cortes <larce@Kings-MacBook-Pro.local>
2019-08-18 13:56:57 +0900

    Deleted for debugging

--- src/styles/index.js
+++ src/styles/index.js
@@ -3,25 +3,4 @@ import globalStyle from '@styles/globalStyle';
 import mixins from '@styles/mixins';
 import mediaQuery from '@styles/mediaQuery';
 
-// Styled components
-import Header from '@styles/components/Header';
-import Main from '@styles/components/Main';
-import Section from '@styles/components/Section';
-import Footer from '@styles/components/Footer';
-import Heading from '@styles/components/Heading';
-import Button from '@styles/components/Button';
-import InlineLink from '@styles/components/InlineLink';
-
-export {
-  theme,
-  globalStyle,
-  mixins,
-  mediaQuery,
-  Header,
-  Main,
-  Section,
-  Footer,
-  Heading,
-  Button,
-  InlineLink
-};
+export { theme, globalStyle, mixins, mediaQuery };
