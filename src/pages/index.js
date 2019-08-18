424f2d0696b7fed039b4ae9122b500fae14e2dbf
King Larce Cortes <kingcort01@gmail.com>
2019-08-18 15:28:49 +0900

    Fox the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Main, mixins } from '@styles';
+
+const MainContainer = styled(Main)`
+  ${mixins.sidePadding};
+  counter-reset: section;
+`;
 
 const IndexPage = ({ data }) => {
+  console.log('DATA : ', data);
   return (
-    <div>
-      Main
-      {data}
-    </div>
+    <MainContainer>
+      <div>Main</div>
+    </MainContainer>
   );
 };
 
@@ -16,3 +24,21 @@ IndexPage.propTypes = {
 };
 
 export default IndexPage;
+
+export const pageQuery = graphql`
+  {
+    hero: allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/hero/" } }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            name
+            contactText
+            subtitles
+          }
+          html
+        }
+      }
+    }
+  }
+`;
