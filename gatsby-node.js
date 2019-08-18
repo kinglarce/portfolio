c4d78049159a6bfbf8333532d1111546e2532fd2
King Larce Cortes <kingcort01@gmail.com>
2019-08-18 15:35:21 +0900

    Adjust again

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+const path = require('path');
+
 exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
   if (stage === 'build-html') {
     actions.setWebpackConfig({
@@ -17,4 +19,20 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
       }
     });
   }
+
+  actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@config': path.resolve(__dirname, 'src/config'),
+        '@content': path.resolve(__dirname, 'src/content'),
+        '@fonts': path.resolve(__dirname, 'src/fonts'),
+        '@images': path.resolve(__dirname, 'src/images'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@styles': path.resolve(__dirname, 'src/styles'),
+        '@utils': path.resolve(__dirname, 'src/utils'),
+        '@static': path.resolve(__dirname, 'static')
+      }
+    }
+  });
 };
