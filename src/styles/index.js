bc9804384405a0b11e963b28740a5da00e8b5efc
King Larce Cortes <larce@Kings-MacBook-Pro.local>
2019-08-18 14:11:56 +0900

    Added styled component

--- src/styles/index.js
+++ src/styles/index.js
@@ -2,5 +2,6 @@ import theme from '@styles/theme';
 import globalStyle from '@styles/globalStyle';
 import mixins from '@styles/mixins';
 import mediaQuery from '@styles/mediaQuery';
+import Button from '@styles/components/Button';
 
-export { theme, globalStyle, mixins, mediaQuery };
+export { theme, globalStyle, mixins, mediaQuery, Button };
