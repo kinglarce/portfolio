81790205eff477d449a44bd1c1eac0171dc6c053
King Larce Cortes <larce@Kings-MacBook-Pro.local>
2019-08-18 14:21:13 +0900

    Add in components to know

---
+++ src/components/Icons/index.js
@@ -0,0 +1,17 @@
+import IconGithub from '@components/Icons/github';
+import IconLinkedin from '@components/Icons/linkedin';
+import IconInstagram from '@components/Icons/instagram';
+import IconExternal from '@components/Icons/external';
+import IconLocation from '@components/Icons/location';
+import IconFacebook from '@components/Icons/facebook';
+import FormattedIcon from '@components/Icons/formattedIcon';
+
+export {
+  IconGithub,
+  IconLinkedin,
+  IconInstagram,
+  IconExternal,
+  IconLocation,
+  IconFacebook,
+  FormattedIcon
+};
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import Email from '@components/Email';
+import Footer from '@components/Footer';
+import Head from '@components/Head';
+import Layout from '@components/Layout';
+import Menu from '@components/Menu';
+import Nav from '@components/Nav';
+import Social from '@components/Social';
+
+export { Email, Footer, Head, Layout, Menu, Nav, Social };
